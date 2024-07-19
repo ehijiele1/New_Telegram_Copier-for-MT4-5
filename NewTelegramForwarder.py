869bf08c115e262e28b144ed964fe0c930b150c7
ehijiele1 <ehijiele1@gmail.com>
2024-07-19 15:25:34 +0100

    Update NewTelegramForwarder.py

--- NewTelegramForwarder.py
+++ NewTelegramForwarder.py
@@ -1,7 +1,5 @@
-import time
 import asyncio
 from telethon.sync import TelegramClient
-from telethon.errors import SessionPasswordNeededError
 
 class TelegramForwarder:
     def __init__(self, api_id, api_hash, phone_number):
@@ -9,98 +7,88 @@ class TelegramForwarder:
         self.api_hash = api_hash
         self.phone_number = phone_number
         self.client = TelegramClient('session_' + phone_number, api_id, api_hash)
+        self.groups = [
+            {"source_id": -1001444264665, "dest_id": -4262620945, "criteria": ["buy", "sell"]},
+            {"source_id": -1001251900924, "dest_id": -4228780769, "criteria": ["buy", "sell"]},
+            {"source_id": -1001761740065, "dest_id": -4246706330, "criteria": ["buy", "sell"]},
+            {"source_id": -1001350478111, "dest_id": -4265309118, "criteria": ["buy", "sell"]},
+            {"source_id": -1001780474473, "dest_id": -4104558322, "criteria": ["buy now", "sell now"]},
+            {"source_id": -1001240888164, "dest_id": -4242163102, "criteria": ["buy", "sell"]},
+            {"source_id": -1001276577932, "dest_id": -4260221938, "criteria": ["sold", "bought"]},
+            {"source_id": -1001276577932, "dest_id": -4228838041, "criteria": ["buy", "sell"]},
+            {"source_id": -1001466079489, "dest_id": -1002159497316, "criteria": ["buy", "sell"]}
+        ]
 
-    async def list_chats(self):
+    async def forward_messages(self):
         await self.client.connect()
 
         # Ensure you're authorized
         if not await self.client.is_user_authorized():
             await self.client.send_code_request(self.phone_number)
-            try:
-                await self.client.sign_in(self.phone_number, input('Enter the code: '))
-            except SessionPasswordNeededError:
-                password = input('Two-step verification enabled. Please enter your password: ')
-                await self.client.sign_in(password=password)
-
-        # Get a list of all the dialogs (chats)
-        dialogs = await self.client.get_dialogs()
-        with open("AllChatsIDs.txt", "w", encoding="utf-8") as chats_file:
-            # Print information about each chat
-            for dialog in dialogs:
-                chats_file.write(f"Chat ID: {dialog.id}, Title: {dialog.title}\n")
-
-        print("List of groups saved to AllChatsIDs.txt successfully!")
-
-    async def forward_messages_to_channel(self, source_chat_id, destination_channel_id, keywords):
-        await self.client.connect()
-
-        # Ensure you're authorized
-        if not await self.client.is_user_authorized():
-            await self.client.send_code_request(self.phone_number)
-            try:
-                await self.client.sign_in(self.phone_number, input('Enter the code: '))
-            except SessionPasswordNeededError:
-                password = input('Two-step verification enabled. Please enter your password: ')
-                await self.client.sign_in(password=password)
-
-        last_message_id = (await self.client.get_messages(source_chat_id, limit=1))[0].id
+            await self.client.sign_in(self.phone_number, input('Enter the code: '))
 
         while True:
-            print("Checking for messages and forwarding them...")
-            # Get new messages since the last checked message
-            messages = await self.client.get_messages(source_chat_id, min_id=last_message_id, limit=None)
+            for group in self.groups:
+                source_chat_id = group["source_id"]
+                destination_channel_id = group["dest_id"]
+                keywords = group["criteria"]
 
-            for message in reversed(messages):
-                # Check if the message text includes any of the keywords
-                if keywords:
-                    if message.text and any(keyword in message.text.lower() for keyword in keywords):
+                last_message_id = (await self.client.get_messages(source_chat_id, limit=1))[0].id
+
+                # Get new messages since the last checked message
+                messages = await self.client.get_messages(source_chat_id, min_id=last_message_id, limit=None)
+
+                for message in reversed(messages):
+                    # Check if the message text includes any of the keywords
+                    if message.text and any(keyword.lower() in message.text.lower() for keyword in keywords):
                         print(f"Message contains a keyword: {message.text}")
 
                         # Forward the message to the destination channel
                         await self.client.send_message(destination_channel_id, message.text)
 
                         print("Message forwarded")
-                else:
-                    # Forward the message to the destination channel
-                    await self.client.send_message(destination_channel_id, message.text)
 
-                    print("Message forwarded")
-
-                # Update the last message ID
-                last_message_id = max(last_message_id, message.id)
+                    # Update the last message ID
+                    last_message_id = max(last_message_id, message.id)
 
             # Add a delay before checking for new messages again
             await asyncio.sleep(5)  # Adjust the delay time as needed
 
 
+# Function to read credentials from file
+def read_credentials():
+    try:
+        with open("credentials.txt", "r") as file:
+            lines = file.readlines()
+            api_id = lines[0].strip()
+            api_hash = lines[1].strip()
+            phone_number = lines[2].strip()
+            return api_id, api_hash, phone_number
+    except FileNotFoundError:
+        print("Credentials file not found.")
+        return None, None, None
+
+# Function to write credentials to file
+def write_credentials(api_id, api_hash, phone_number):
+    with open("credentials.txt", "w") as file:
+        file.write(api_id + "\n")
+        file.write(api_hash + "\n")
+        file.write(phone_number + "\n")
+
 async def main():
-    # Hardcoded credentials
-    api_id = '22877272'
-    api_hash = '7495591e1824a05f02b57d92c2d15bdd'
-    phone_number = '+2348024427735'
+    # Attempt to read credentials from file
+    api_id, api_hash, phone_number = read_credentials()
+
+    # If credentials not found in file, prompt the user to input them
+    if api_id is None or api_hash is None or phone_number is None:
+        api_id = input("Enter your API ID: ")
+        api_hash = input("Enter your API Hash: ")
+        phone_number = input("Enter your phone number: ")
+        # Write credentials to file for future use
+        write_credentials(api_id, api_hash, phone_number)
 
     forwarder = TelegramForwarder(api_id, api_hash, phone_number)
-    
-    print("Choose an option:")
-    print("1. List Chats")
-    print("2. Forward Messages")
-    
-    choice = input("Enter your choice: ")
-    
-    if choice == "1":
-        await forwarder.list_chats()
-    elif choice == "2":
-        source_chat_id = int(input("Enter the source chat ID: "))
-        destination_channel_id = int(input("Enter the destination chat ID: "))
-        print("Enter keywords if you want to forward messages with specific keywords, or leave blank to forward every message!")
-        keywords = input("Put keywords (comma separated if multiple, or leave blank): ").split(",")
-
-        # Remove empty keyword if no keywords are provided
-        keywords = [keyword.strip() for keyword in keywords if keyword.strip()]
-
-        await forwarder.forward_messages_to_channel(source_chat_id, destination_channel_id, keywords)
-    else:
-        print("Invalid choice")
+    await forwarder.forward_messages()
 
 # Start the event loop and run the main function
 if __name__ == "__main__":
